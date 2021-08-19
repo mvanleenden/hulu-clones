3f83999ae4d087ac508c1497e1bbd4261675c7f4
Mayron van Leenden <mvanleenden@gmail.com>
2021-08-19 05:42:05 +0200

    navigation component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +11,7 @@ const Home: NextPage = () => {
         <title>Hulu</title>
       </Head>
       <Header />
+      <Nav />
       {/* NavBar */}
       {/* Results */}
     </div>
